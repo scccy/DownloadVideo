f0905a6eff541d80d3a5f800829696b0d7c86d2a
scccy <shici2880@gmail.com>
2025-02-01 01:04:36 +0800

    refactor(common-core): 重构通用配置和初始化代码
    - 更新 CommonConfig、DatabaseInitConfig 等配置类
    - 重构 FastJson2Config，使用 FastJsonHttpMessageConverter
    - 修改 FeignConfig 中的重试策略
    - 更新 Knife4jConfig，使用 OpenAPI配置
    - 删除 TraceIdFilter 类
    - 移除 WebFluxConfig 和 WebMvcConfig 文件- 更新 pom.xml 中的依赖版本

--- common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/CommonConfig.java
+++ common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/CommonConfig.java
@@ -1,6 +1,7 @@
 package com.scccy.downloadvideo.common.core.config;
 
 import com.scccy.downloadvideo.common.core.utils.TraceIdUtil;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/Knife4jConfig.java
+++ common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/Knife4jConfig.java
@@ -1,17 +1,30 @@
 package com.scccy.downloadvideo.common.core.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Knife4jConfig {
-    
+
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public-api")
-                .pathsToMatch("/api/**")
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("视频下载平台 API")
+                        .version("1.0")
+                        .description("视频下载平台接口文档")
+                        .termsOfService("https://scccy.com")
+                        .contact(new Contact()
+                                .name("scccy")
+                                .email("contact@scccy.com")
+                                .url("https://scccy.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                );
     }
 } 
