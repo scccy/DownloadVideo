52f1179a50c0c10daab5799e481c41bb2040a801
scccy <shici2880@gmail.com>
2025-01-31 23:56:26 +0800

    refactor: 重构项目并暂时移除 WebSocket 功能
    - 更新 .gitignore 文件
    - 重构 Assert 类并添加包声明
    - 暂时注释掉 BaseCrawler 中的 handleHttpError 方法
    - 注释掉 BaseWebSocketListener 类
    - 更新 CommonAutoConfiguration 类，移除 OkHttpClient Bean
    -重构 DatabaseInitConfig 类
    - 更新 DouyinLiveListener 类
    - 删除 GlobalExceptionHandler 类
    - 更新 Response 类
    - 注释掉 WebSocketAutoConfiguration 类
    - 注释掉 WebSocketClient 类
    - 注释掉 WebSocketService 类
    - 更新项目依赖，添加 spring-webfeat: 新增抖音相关服务与控制器，并调整部分配置文件及移除无用代码

--- common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/CommonAutoConfiguration.java
+++ common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/CommonAutoConfiguration.java
@@ -13,10 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor;
-import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableConfigurationProperties({
@@ -24,7 +20,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
     WebSocketProperties.class
 })
 @ComponentScan(basePackages = "com.scccy.downloadvideo.common.core")
-public class CommonAutoConfiguration implements WebFluxConfigurer {
+public class CommonAutoConfiguration {
     
     @Bean
     @ConditionalOnMissingBean
@@ -45,14 +41,4 @@ public class CommonAutoConfiguration implements WebFluxConfigurer {
         return new ConfigurationPropertiesBindingPostProcessor();
     }
 
-    @Override
-    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
-        ObjectMapper mapper = objectMapper();
-        configurer.defaultCodecs().jackson2JsonEncoder(
-            new Jackson2JsonEncoder(mapper)
-        );
-        configurer.defaultCodecs().jackson2JsonDecoder(
-            new Jackson2JsonDecoder(mapper)
-        );
-    }
 } 
