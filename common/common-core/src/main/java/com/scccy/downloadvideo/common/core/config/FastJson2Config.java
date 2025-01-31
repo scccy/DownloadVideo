aa5a8ae3571c5ad12282f01bcd577fa8f88cbc32
scccy <shici2880@gmail.com>
2025-02-01 00:47:32 +0800

    refactor: 重构项目并暂时移除 WebSocket 功能
    - 更新 .gitignore 文件
    - 重构 Assert 类并添加包声明
    - 暂时注释掉 BaseCrawler 中的 handleHttpError 方法
    - 注释掉 BaseWebSocketListener 类
    - 更新 CommonAutoConfiguration 类，移除 OkHttpClient Bean
    -重构 DatabaseInitConfig 类
    - 更新 DouyinLiveListener 类
    - 删除 GlobalExceptionHandler 类
    - 更新 Response 类
    - 注释掉 WebSocketAutoConfiguration 类
    - 注释掉 WebSocketClient 类
    - 注释掉 WebSocketService 类
    - 更新项目依赖，添加 spring-webfeat: 新增抖音相关服务与控制器，并调整部分配置文件及移除无用代码

--- common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/FastJson2Config.java
+++ common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/FastJson2Config.java
@@ -1,6 +1,6 @@
 package com.scccy.downloadvideo.common.core.config;
 
-import com.alibaba.fastjson2.support.spring6.data.reactive.FastJsonHttpMessageWriter;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
--- common/common-core/src/main/java/com/scccy/downloadvideo/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.config.EnableWebFlux;
-
-@Configuration
-@EnableWebFlux
-public class WebConfig implements WebFluxConfigurer {
-    // WebFlux 相关配置
-} 
