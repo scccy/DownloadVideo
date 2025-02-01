2707e15f7c7b2c74931ddb3f403eac1f8d36d006
scccy <shici2880@gmail.com>
2025-02-01 21:44:49 +0800

    refactor(common-core): 重构通用核心模块
    - 修改包名：将 config.manager 改为 utils.manager
    - 删除未使用的 BarkConfig 类
    - 注释掉 BaseDownloader 类
    - 移除 CommonAutoConfiguration 和 CommonConfig 类
    - 更新阿波龙操作切面的引用- 调整日志级别配置
    - 导入 Nacos 配置

--- platform-services/douyin-service/src/main/java/com/scccy/downloadvideo/platform/douyin/service/DouyinDownloadConfigService.java
+++
@@ -1,8 +0,0 @@
-package com.scccy.downloadvideo.platform.douyin.service;
-
-import com.scccy.downloadvideo.platform.douyin.model.DouyinDownloadConfig;
-import com.baomidou.mybatisplus.extension.service.IService;
-public interface DouyinDownloadConfigService extends IService<DouyinDownloadConfig>{
-
-
-}
--- platform-services/douyin-service/src/main/java/com/scccy/downloadvideo/platform/douyin/service/DouyinWebcastSignatureService.java
+++
@@ -1,8 +0,0 @@
-package com.scccy.downloadvideo.platform.douyin.service;
-
-import com.scccy.downloadvideo.platform.douyin.model.DouyinWebcastSignature;
-import com.baomidou.mybatisplus.extension.service.IService;
-public interface DouyinWebcastSignatureService extends IService<DouyinWebcastSignature>{
-
-
-}
