7e0d782b0e75cc580e244b89499115216100cb17
scccy <shici2880@gmail.com>
2025-02-01 12:34:07 +0800

    feat: 添加基础配置和工具类
    - 新增数据库配置、MyBatis Plus 配置、日志配置等基础配置
    - 添加自定义日志配置和抖音相关配置
    - 配置 Feign 客户端
    - 新增 WebConfig 配置类，启用 WebFlux 并配置消息编解码器
    - 添加全局导入配置文件

--- common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/WebConfig.java
+++ common/common-core/src/main/java/com/scccy/downloadvideo/common/core/config/WebConfig.java
@@ -1,39 +1,29 @@
 package com.scccy.downloadvideo.common.core.config;
 
-import com.alibaba.fastjson2.support.config.FastJsonConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 @EnableWebFlux
-@Order(1)
 public class WebConfig implements WebFluxConfigurer {
+    
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+    
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean() {
+        return new LocalValidatorFactoryBean();
+    }
 
     @Override
     public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
-        configurer.registerDefaults(false);
-        //反序列化
-        configurer.customCodecs().register(new FastjsonConfig.CustomDecoder());
-        //序列化
-        configurer.customCodecs().register(new FastjsonConfig.CustomEncoder());
+        configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024); // 设置最大内存缓冲为16MB
     }
-
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
-}
-
-
+} 
